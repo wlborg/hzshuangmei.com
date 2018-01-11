b5c7dd244b7441a7b97c2c527d5d0990b45ebbf6
chaihongjun.me <441315628@qq.com>
2018-01-11 11:22:42 +0800

    repair imgNotFound is not defined
    repair imgNotFound is not defined

--- static.hzshuangmei.com/js/m/kst_popup.js
+++ static.hzshuangmei.com/js/m/kst_popup.js
@@ -4,7 +4,7 @@
 * @Date:   2018-01-06 10:54:26
 * @Last Modified by:   chj
 
-* @Last Modified time: 2018-01-11 11:14:09
+* @Last Modified time: 2018-01-11 11:20:36
 
 */
 /*  移动版     自定义弹窗邀请框 */
@@ -16,17 +16,17 @@
 1. <script src="{dede:global.cfg_jspath/}/pc/layer/mobile/layer.js"></script> 引入
 2. 配置
  */
-function popup() {
-    //get current page filename
-    var target = window.location.href;
-    var filename = window.location.pathname.split("/")[window.location.pathname.split("/").length - 1].split(".")[0];
-    var timer = null;
 function imgNotFound() {
         var img = $(".popup img").get(0);
         //默认图片
         img.src = "//img.hzshuangmei.com/pc/kst/default.png";
         img.onerror = null;
     }
+function popup() {
+    //get current page filename
+    var target = window.location.href;
+    var filename = window.location.pathname.split("/")[window.location.pathname.split("/").length - 1].split(".")[0];
+    var timer = null;
     //不永远关闭弹窗
     var flag = 0;
     layer.open({
