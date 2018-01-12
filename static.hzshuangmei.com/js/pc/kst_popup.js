076e86b45796861a16754a68a600d721b5d94afa
chaihongjun.me <441315628@qq.com>
2018-01-12 14:07:31 +0800

    报错bug
    报错bug

--- static.hzshuangmei.com/js/pc/kst_popup.js
+++ static.hzshuangmei.com/js/pc/kst_popup.js
@@ -3,7 +3,7 @@
  * @FileName:kst_popup.js
  * @Date:   2018-01-06 11:05:43
  * @Last Modified by:   chj
- * @Last Modified time: 2018-01-11 13:49:35
+ * @Last Modified time: 2018-01-12 14:06:03
  */
 /* PC版   自定义弹窗邀请框 */
 /*
@@ -20,7 +20,6 @@ function imgNotFound() {
         //默认图片
         img.src = "//img.hzshuangmei.com/pc/kst/default.png";
         img.onerror = null;
-        return true;
     }
 function popup() {
     //get current page filename
