011255ea880db9abcf5d9775d397af6e142ae7cf
chaihongjun.me <441315628@qq.com>
2018-01-19 08:51:52 +0800

    修改案例图大图预加载方式
    修改案例图大图预加载方式

--- static.hzshuangmei.com/js/pc/article_common.js
+++ static.hzshuangmei.com/js/pc/article_common.js
@@ -29,8 +29,8 @@ var currentFileName=window.location.pathname.split("/")[window.location.pathname
 var currentID=currentFileName.split(".")[0];
 var BigImgsNum=$(".main").find("img").length;
 for (var i = 1 ; i <BigImgsNum+1; i++) {
-            $("head").append('<link rel="prefetch" href="https://uploads.hzshuangmei.com/bigcaseimage/'+currentID+'/'+i+'.jpg">');
-             $("head").append('<link rel="prerender" href="https://uploads.hzshuangmei.com/bigcaseimage/'+currentID+'/'+i+'.jpg">');
+            $("head").append('<link rel="preload" as="image" href="https://uploads.hzshuangmei.com/bigcaseimage/'+currentID+'/'+i+'.jpg">');
+            $("head").append('<link rel="prerender" href="https://uploads.hzshuangmei.com/bigcaseimage/'+currentID+'/'+i+'.jpg">');
  }
 }
 $(function() {
