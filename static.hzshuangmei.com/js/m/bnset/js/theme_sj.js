e98c702b20a7c0a2efe13a6a77a8d866b6a3ccc8
August1Rush <271945920@qq.com>
2018-01-24 16:55:37 +0800

    玻尿酸额头手机
    玻尿酸额头手机

---
+++ static.hzshuangmei.com/js/m/bnset/js/theme_sj.js
@@ -0,0 +1,155 @@
+function XZSlide(options) {
+    if (typeof options !== 'object') return false;
+    this.speed = options.speed || 500;
+    this.interval = options.interval || 3000;
+    this.step = options.itemWidth || 0;
+    this.slideWEle = options.cover || null;
+    this.slideCEle = options.container || null;
+    this.itemCount = options.itemCount || null;
+    this.num = 0;
+    this.nextEle = options.nextBtn || null;
+    this.prevEle = options.prevBtn || null;
+    this.indicator = options.indicator || null;
+    this.auto = options.auto || false;
+    this.resp = options.resp || false;
+    this.timer = 0;
+    this.init();
+}
+XZSlide.prototype.init = function() {
+    var self = this;
+    if (this.slideCEle == null) return false;
+    if (this.resp) {
+        this.step = parseInt(this.slideWEle.css("width"));
+    }
+    this.slideCEle.css("position", "relative");
+    if (this.itemCount == null) {
+        this.itemCount = this.slideCEle.find(".item").length;
+    }
+    this.slideCEle.find(".item:first").clone().appendTo(this.slideCEle);
+    this.slideCEle.css("width", (this.itemCount + 1) * this.step+20 + "px");
+    if (this.auto) {
+        this.timer = setInterval($.proxy(this.next, this), this.interval);
+        this.timerControl(this.slideWEle);
+    }
+    if (this.nextEle) {
+        this.timerControl(this.nextEle);
+        this.nextEle.click(function() {
+            self.next();
+        });
+    }
+    if (this.prevEle) {
+        this.timerControl(this.prevEle);
+        this.prevEle.click(function() {
+            self.prev();
+        });
+    }
+    if (this.indicator) {
+        this.timerControl(this.indicator);
+        this.indicator.find("li").click(function() {
+            var index = $(this).index();
+            // $(this).addClass("active").siblings().removeClass("active");
+            self.next(index + 1); //传入参数为0的话，next函数接收的值为undefined,原因未知。
+        });
+    }
+};
+XZSlide.prototype.timerControl = function(ele) {
+    var self = this;
+    ele.mouseover(function(e) {
+        var event = window.event || e;
+        event.stopPropagation();
+        clearTimeout(self.timer);
+    }).mouseout(function(e) {
+        var event = window.event || e;
+        event.stopPropagation();
+        self.timer = setInterval($.proxy(self.next, self), self.interval);
+    });
+};
+XZSlide.prototype.next = function(number) {
+    //indicator支持
+    if (number) {
+        this.num = number - 1;
+    } else {
+        this.num += 1;
+    }
+    if (this.num > this.itemCount) {
+        var length = -this.step;
+        this.slideCEle.css({
+            left: 0
+        }).stop().animate({
+            left: length
+        }, this.speed);
+        this.num = 1;
+    } else {
+        var length = -this.num * this.step;
+        this.slideCEle.stop().animate({
+            left: length
+        }, this.speed);
+    }
+    //indicator支持
+    if (this.indicator) {
+        this.indi();
+    }
+};
+XZSlide.prototype.prev = function() {
+    this.num -= 1;
+    if (this.num < 0) {
+        var length1 = -this.step * (this.itemCount);
+        var length2 = -this.step * (this.itemCount - 1);
+        this.slideCEle.css({
+            left: length1
+        }).stop().animate({
+            left: length2
+        }, this.speed);
+        this.num = this.itemCount - 1;
+    } else {
+        var length = -this.num * this.step;
+        this.slideCEle.stop().animate({
+            left: length
+        }, this.speed);
+    }
+    //indicator支持
+    if (this.indicator) {
+        this.indi();
+    }
+};
+XZSlide.prototype.indi = function() {
+    if (this.num > this.itemCount - 1) {
+        this.indicator.find("li").eq(0).addClass("active").siblings().removeClass("active");
+    } else {
+        this.indicator.find("li").eq(this.num).addClass("active").siblings().removeClass("active");
+    }
+}
+$(function() {
+    var slide1 = new XZSlide({
+        cover: $(".m2 .slideW"),
+        container: $(".m2 .slideC"),
+        itemCount: 3,
+        // itemWidth: 631,
+        speed: 300,
+        auto: true,
+        resp:true,
+        nextBtn: $(".m2 .next"),
+        prevBtn: $(".m2 .prev")
+    });
+    var slide2 = new XZSlide({
+        cover: $(".m5 .slideW"),
+        container: $(".m5 .slideC"),
+        itemCount: 2,
+        // itemWidth: 746,
+        speed: 300,
+        auto: true,
+        resp:true,
+        nextBtn: $(".m5 .next"),
+        prevBtn: $(".m5 .prev"),
+        indicator: $(".m5 .indicator")
+    });
+    var slide3 = new XZSlide({
+        cover: $(".m6 .slideW"),
+        container: $(".m6 .slideC"),
+        itemCount: 3,
+        // itemWidth:640,
+        resp:true,
+        speed: 300,
+        auto: true
+    });
+});
