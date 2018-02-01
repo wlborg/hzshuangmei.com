90af015d1cf70d894bfea0319a7548dde88af6bb
chaihongjun.me <441315628@qq.com>
2018-02-01 15:41:11 +0800

    取消导航栏的淡入淡出效果
    取消导航栏的淡入淡出效果

--- static.hzshuangmei.com/js/m/common.js
+++ static.hzshuangmei.com/js/m/common.js
@@ -591,74 +591,83 @@
    /*滑动屏幕隐藏和现实导航栏*/
    // 手指在屏幕上滑动的时候顶部的导航栏自动隐藏，只保留离线宝
    //
-   window.onload = function() {
-       //底层共用
-       var iBase = {
-           Id: function(name) {
-               return document.getElementById(name);
-           },
-           //设置元素透明度,透明度值按IE规则计,即0~100
-           SetOpacity: function(ev, v) {
-               ev.filters ? ev.style.filter = 'alpha(opacity=' + v + ')' : ev.style.opacity = v / 100;
-           }
-       }
-       //淡入效果(含淡入到指定透明度)
-       function fadeIn(elem, speed, opacity) {
-           /*
-            * 参数说明
-            * elem==>需要淡入的元素
-            * speed==>淡入速度,正整数(可选)
-            * opacity==>淡入到指定的透明度,0~100(可选)
-            */
-           speed = speed || 20;
-           opacity = opacity || 100;
-           //显示元素,并将元素值为0透明度(不可见)
-           elem.style.display = 'block';
-           iBase.SetOpacity(elem, 0);
-           //初始化透明度变化值为0
-           var val = 0;
-           //循环将透明值以10递增,即淡入效果
-           (function() {
-               iBase.SetOpacity(elem, val);
-               val += 10;
-               if (val <= opacity) {
-                   setTimeout(arguments.callee, speed)
-               }
-           })();
-       }
-       //淡出效果(含淡出到指定透明度)
-       function fadeOut(elem, speed, opacity) {
-           /*
-            * 参数说明
-            * elem==>需要淡出的元素
-            * speed==>淡出速度,正整数(可选)
-            * opacity==>淡出到指定的透明度,0~100(可选)
-            */
-           speed = speed || 20;
-           opacity = opacity || 0;
-           //初始化透明度变化值为0
-           var val = 100;
-           //循环将透明值以10递减,即淡出效果
-           (function() {
-               iBase.SetOpacity(elem, val);
-               val -= 10;
-               if (val >= opacity) {
-                   setTimeout(arguments.callee, speed);
-               } else if (val < 0) {
-                   //元素透明度为0后隐藏元素
-                   elem.style.display = 'none';
-               }
-           })();
-       }
-       document.addEventListener("touchmove", function(e) {
-           // e.preventDefault();
-           fadeOut(iBase.Id('menu'));
-           console.log('执行滑动');
-       }, false)
-       // 当手指离开屏幕的时候，显示导航栏
-       document.addEventListener("touchcancel", function(e) {
-            e.preventDefault();
-           fadeIn(iBase.Id('menu'));
-           console.log('手离开屏幕');
-       }, false)
-   }
+   // window.onload = function() {
+   //     //底层共用
+   //     var iBase = {
+   //         Id: function(name) {
+   //             return document.getElementById(name);
+   //         },
+   //         //设置元素透明度,透明度值按IE规则计,即0~100
+   //         SetOpacity: function(ev, v) {
+   //             ev.filters ? ev.style.filter = 'alpha(opacity=' + v + ')' : ev.style.opacity = v / 100;
+   //         }
+   //     }
+   //     //淡入效果(含淡入到指定透明度)
+   //     function fadeIn(elem, speed, opacity) {
+   //         /*
+   //          * 参数说明
+   //          * elem==>需要淡入的元素
+   //          * speed==>淡入速度,正整数(可选)
+   //          * opacity==>淡入到指定的透明度,0~100(可选)
+   //          */
+   //         speed = speed || 20;
+   //         opacity = opacity || 100;
+   //         //显示元素,并将元素值为0透明度(不可见)
+   //         elem.style.display = 'block';
+   //         iBase.SetOpacity(elem, 0);
+   //         //初始化透明度变化值为0
+   //         var val = 0;
+   //         //循环将透明值以10递增,即淡入效果
+   //         (function() {
+   //             iBase.SetOpacity(elem, val);
+   //             val += 10;
+   //             if (val <= opacity) {
+   //                 setTimeout(arguments.callee, speed)
+   //             }
+   //         })();
+   //     }
+   //     //淡出效果(含淡出到指定透明度)
+   //     function fadeOut(elem, speed, opacity) {
+
+   //          * 参数说明
+   //          * elem==>需要淡出的元素
+   //          * speed==>淡出速度,正整数(可选)
+   //          * opacity==>淡出到指定的透明度,0~100(可选)
+
+   //         speed = speed || 20;
+   //         opacity = opacity || 0;
+   //         //初始化透明度变化值为0
+   //         var val = 100;
+   //         //循环将透明值以10递减,即淡出效果
+   //         (function() {
+   //             iBase.SetOpacity(elem, val);
+   //             val -= 10;
+   //             if (val >= opacity) {
+   //                 setTimeout(arguments.callee, speed);
+   //             } else if (val < 0) {
+   //                 //元素透明度为0后隐藏元素
+   //                 elem.style.display = 'none';
+   //             }
+   //         })();
+   //     }
+
+   //     document.addEventListener("touchstart", function(e) {
+   //         // e.preventDefault();
+   //         fadeOut(iBase.Id('menu'));
+   //         console.log('执行滑动');
+   //     }, false)
+
+
+
+   //     document.addEventListener("touchmove", function(e) {
+   //         // e.preventDefault();
+   //         fadeOut(iBase.Id('menu'));
+   //         console.log('执行滑动');
+   //     }, false)
+   //     // 当手指离开屏幕的时候，显示导航栏
+   //     document.addEventListener("touchend", function(e) {
+   //          e.preventDefault();
+   //         fadeIn(iBase.Id('menu'));
+   //         console.log('手离开屏幕');
+   //     }, false)
+   // }
