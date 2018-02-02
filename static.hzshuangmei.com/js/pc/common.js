968f667774c4af661f5d0d977465c5a856fb9792
juice121 <243551641@qq.com>
2018-02-02 17:04:15 +0800

    项目列表修改7
    项目列表修改7

--- static.hzshuangmei.com/js/pc/common.js
+++ static.hzshuangmei.com/js/pc/common.js
@@ -284,7 +284,7 @@ var effects = (
 $(function() {
     effects.lightCurNav(".nav_ul", "currnet");
     effects.lightCurNav("#case_nav", "currnet1");
-    //effects.lightCurNav("#project_nav", "currnet1");
+    effects.lightCurNav("#project_nav", "currnet2");
     effects.showCaseBigImage(".case-article-modal", '#case-modal-img');
     tools.addKSTScript();
     tools.addBaiduScript();
