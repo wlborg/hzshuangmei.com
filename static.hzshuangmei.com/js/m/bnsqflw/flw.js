615350873d68298ce4f8abe3a1a264aebfabfd8b
August1Rush <271945920@qq.com>
2018-04-11 14:05:40 +0800

    js修改
    js修改

--- static.hzshuangmei.com/js/m/bnsqflw/flw.js
+++ static.hzshuangmei.com/js/m/bnsqflw/flw.js
@@ -1,16 +1,17 @@
+
 var zjindex=1;
 $(".m5jh div").click(function () {
 
     zjindex=$(this).index();
     var qh=zjindex+1;
-    $(".m5qh").attr("src",'./images/m5qh'+qh+'.png');
+    $(".m5qh").attr("src",'//img.hzshuangmei.com/pc/bnsqflw/images/m5qh'+qh+'.png');
     $(".m5zj div").eq($(this).index()).css("display","block").siblings().css("display","none");
 
 });
 function zjqh() {
     $('.m5zj div').eq(zjindex).show().siblings().hide();
     var qh2=zjindex+1;
-    $(".m5qh").attr("src",'./images/m5qh'+qh2+'.png');
+    $(".m5qh").attr("src",'//img.hzshuangmei.com/pc/bnsqflw/images/m5qh'+qh+'.png');
     zjindex++;
     if (zjindex>2){
         zjindex=0
