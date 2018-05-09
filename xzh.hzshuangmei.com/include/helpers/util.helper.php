8cbd044c3245562a4a882a4cbe79174fe49df9c6
nihaoactf <31620183+nihaoactf@users.noreply.github.com>
2018-05-09 17:13:02 +0800

    博客推荐
    博客推荐

--- xzh.hzshuangmei.com/include/helpers/util.helper.php
+++ xzh.hzshuangmei.com/include/helpers/util.helper.php
@@ -378,19 +378,5 @@ if (!function_exists('json_decode')) {
     }
 }
 //==============
-function fix_session_register(){
-function session_register(){
-$args = func_get_args();
-foreach ($args as $key){
-$_SESSION[$key]=$GLOBALS[$key];
-}
-}
-function session_is_registered($key){
-return isset($_SESSION[$key]);
-}
-function session_unregister($key){
-unset($_SESSION[$key]);
-}
-}
-if (!function_exists('session_register')) fix_session_register();
+
 //==============
