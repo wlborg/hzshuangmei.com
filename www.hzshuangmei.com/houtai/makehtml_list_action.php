b0eba351366c41faa8ea3c1be84a3fa989ebe577
chaihongjun.me <441315628@qq.com>
2018-05-25 13:35:40 +0800

    还原
    还原

--- www.hzshuangmei.com/houtai/makehtml_list_action.php
+++ www.hzshuangmei.com/houtai/makehtml_list_action.php
@@ -33,7 +33,7 @@ if($gotype=='' || $gotype=='mkallct')
     {
         //if($typeid>0)
         //排除熊掌号栏目id=76
-        if(($typeid>0)&&($typeid!=76))
+        if($typeid>0)
         {
             $tidss = GetSonIds($typeid,0);
             $idArray = explode(',',$tidss);
