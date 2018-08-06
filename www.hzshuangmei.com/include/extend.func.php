1a70a0a6cb1c4b6f10267428876c5174423cefa3
August1Rush <271945920@qq.com>
2018-08-06 10:18:15 +0800

    推荐资讯加https图片路径
    推荐资讯加https图片路径

--- www.hzshuangmei.com/include/extend.func.php
+++ www.hzshuangmei.com/include/extend.func.php
@@ -1252,6 +1252,7 @@ $urlarray = GetOneArchive($id);
 $url = $urlarray['arcurl'];
 
 $litpic =$row["litpic"];
+$relateproject.='<li class="expert"><a href="'.$url.'" target="_blank" rel="nofollow"><span class="thumbnail"><img src="https://xzh.hzshuangmei.com'.$litpic.'" alt="'.$title.'"></span><span class="expertSpan line-limit-length">'.$title.'</span><img  src="//img.hzshuangmei.com/pc/option.png" alt="除皱针有哪些优点？" class="thumbnailOp"></span></a></li>';
 }
 if($ns>0){
 $relateproject=$relateproject;
