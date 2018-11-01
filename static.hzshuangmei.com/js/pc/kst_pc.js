7d89256f56b595e1ec0b745b0a2b1040a294e3bb
August1Rush <271945920@qq.com>
2018-11-01 16:22:07 +0800

    隐藏
    隐藏

--- static.hzshuangmei.com/js/pc/kst_pc.js
+++ static.hzshuangmei.com/js/pc/kst_pc.js
@@ -7,7 +7,7 @@
             if(dom) {
                   // 点击稍后咨询按钮隐藏，12秒后出现
                  $(document).on("click","#div3",function(){
-                    $("#k_s_ol_inviteWin").css("display","none!important");
+                    $("#k_s_ol_inviteWin").hide();
                  });
                 //  清除定时器
                 if(!timer) {
