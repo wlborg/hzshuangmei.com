edc16e47da54e69eb8529d14d88e33750dd64ead
August1Rush <271945920@qq.com>
2018-11-07 11:32:05 +0800

    快商通
    快商通

--- static.hzshuangmei.com/js/m/kst_pc.js
+++ static.hzshuangmei.com/js/m/kst_pc.js
@@ -30,3 +30,30 @@
 
 
 
+        var timer2 = null
+            //  检查dom是否执行完成
+        function check2() {
+            var dom = document.getElementById('div1')
+            if(dom) {
+
+                  // 点击稍后咨询按钮隐藏，12秒后出现
+                 $(document).on("click","#div1",function(){
+                    $("#_ks_ol_inviteWin").attr("id","kReomve");
+
+                 });
+                    $(document).on("click","#div1",function(){
+                    $("#_ks_ol_inviteWin").attr("id","kReomve");
+
+                 });
+                //  清除定时器
+                if(!timer2) {
+                    clearTimeout(timer2)
+                }
+            } else {
+                //  自我调用
+                timer2 = setTimeout(check2, 0)
+            }
+        }
+        //  首次执行
+        check2();
+
