517e2c2f31b332d18fbb014bf275617eb5a8a146
August1Rush <271945920@qq.com>
2019-04-16 18:02:44 +0800

    案例
    案例

--- static.hzshuangmei.com/js/pc/common.js
+++ static.hzshuangmei.com/js/pc/common.js
@@ -275,14 +275,15 @@ var effects = (
                 casemodal.removeClass('case-show');
                 casemodalimg.attr('src', "");
             })
-            //casetargetimgs.each(function(index) {
-            //    $(this).on('click', function() {
-            //        var num = index + 1;
-            //        casecurrentbigimgnum = num;
-            //        addimgurl(num);
-            //        casemodal.addClass('case-show');
-            //    })
-            //});
+            casetargetimgs.each(function(index) {
+                console.log($(this).attr("class"));
+                $(this).on('click', function() {
+                    var num = index + 1;
+                    casecurrentbigimgnum = num;
+                    addimgurl(num);
+                    casemodal.addClass('case-show');
+                })
+            });
             casenextbtn.click(function() {
                 var num = casecurrentbigimgnum + 1;
                 if (num > casetargetimgslength) {
