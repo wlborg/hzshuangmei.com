0df4c4fa7956da98596fe047a9104c18d92a7792
August1Rush <271945920@qq.com>
2019-04-19 15:46:53 +0800

    帮助中心获取医生头像
    帮助中心获取医生头像

--- www.hzshuangmei.com/include/extend.func.php
+++ www.hzshuangmei.com/include/extend.func.php
@@ -2167,23 +2167,24 @@ return $repairRes;
 *  帮助中心详情页读取医生的名字,根据文档填写医生的文档id
 *  $typeid     当前栏目id
 */
-function getDoctorHeadFormId($typeid)
+function getDoctorHeadFormId($doctorid)
 {
 global $dsql;
-$repairRes="";
-$relateproject="";
-$repair=0;
-$dsql->SetQuery( "SELECT * FROM #@__archives AS a,#@__addondoctors AS b where a.id =b.aid  and a.id =135 and a.arcrank=0  order by rand() limit 1;");
-$dsql->Execute();
-$ns = $dsql->GetTotalRow();
-{
-$id = $row["id"];
-$title = cn_substr($row["title"],80,0);
-$shorttitle = cn_substr($row["shorttitle"],80,0);
-$urlarray = GetOneArchive($id);
-$url = $urlarray['arcurl'];
-$litpic =replaceurl($row["litpic"]);
-$relateproject.='<img src="'.$litpic.'" alt="'.$litpic.'" />';
+$relateInfo="";
+if($doctorid!=""){
+      $dsql->SetQuery( "SELECT * FROM #@__archives AS a,#@__addondoctors AS b where a.id =b.aid  and a.id = '$doctorid' and a.arcrank=0  order by rand() limit 1");
+      $dsql->Execute();
+      $ns = $dsql->GetTotalRow();
+      while($row=$dsql->GetArray())
+      {
+      $litpic =replaceurl($row["litpic"]);
+      $relateInfo.='<img src="'.$litpic.'" alt="'.$litpic.'" />';
+      }
+      if($ns>0){
+      $relateInfo=$relateInfo;
+      }
+}else{
+    $relateInfo.="<img src="https://img.hzshuangmei.com/pc/tl_logo.png" alt="默认客服头像" />";
 }
- return $relateproject;
+ return $relateInfo;
 }
