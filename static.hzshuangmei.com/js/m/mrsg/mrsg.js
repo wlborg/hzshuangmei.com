6b60e7fe343f3ee657e3cd3d0bfc3463ba6b5457
lidanji8 <993870144@qq.com>
2019-08-13 17:14:40 +0800

    美人水光css0.3
    美人水光css0.3

---
+++ static.hzshuangmei.com/js/m/mrsg/mrsg.js
@@ -0,0 +1,14 @@
+$(function () {
+    var mySwiper1 = new Swiper('.box7_swiper', {
+        direction: 'horizontal',
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        loop:true,
+        pagination: {
+            el: '.pagination2',
+        },
+    })
+})
+
