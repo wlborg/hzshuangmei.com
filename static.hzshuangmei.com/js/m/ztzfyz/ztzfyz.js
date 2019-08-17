febf8d5ef08237c4e1892370d577aff8e0687c53
lidanji8 <993870144@qq.com>
2019-08-17 09:40:30 +0800

    自体脂肪移植移动端
    自体脂肪移植移动端

---
+++ static.hzshuangmei.com/js/m/ztzfyz/ztzfyz.js
@@ -0,0 +1,14 @@
+$(function () {
+    var mySwiper1 = new Swiper('.box6_swiper', {
+        direction: 'horizontal',
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        loop:true,
+        pagination: {
+            el: '.pagination2',
+        },
+    })
+})
+
